12ddaa84666f63d5df204a09fc901e8c762bf09b
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2025-06-11 02:46:07 +0900

    add: vite config

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        result: path.resolve(__dirname, 'public/result.html'),
+      },
+    },
+  },
+})
