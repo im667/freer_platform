aa0ad9b6d0b21261e537b8db1d6cc0115f630d1b
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2025-06-12 19:57:36 +0900

    add: meta-pixel js

--- vite.config.ts
+++ vite.config.ts
@@ -18,25 +18,8 @@ export default defineConfig({
             injectTo: 'head', // head에 넣는다
             tag: 'script',
             attrs: {
-              // 여기에 src 쓰면 외부파일, 아니면 innerHTML로 삽입
+               src: '/meta-pixel.js'
             },
-            children: `
-              <!-- Meta Pixel Code -->
-          
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '617665268001613');
-              fbq('track', 'PageView');
-           
-            
-              <!-- End Meta Pixel Code -->
-            `.trim()
           },
           {
             injectTo: 'body',
