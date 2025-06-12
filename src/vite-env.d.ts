6b083a98da750241e0ceef2ba79d4cf891f3e518
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2025-06-12 18:51:05 +0900

    fix: key change

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_API_KEY2: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
