8be8070ee8cb9003c4c66b549bc7e117d27eabe2
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2025-06-12 20:04:51 +0900

    fix: pixel 직접삽입

--- vite.config.ts
+++ vite.config.ts
@@ -18,17 +18,8 @@ export default defineConfig({
             injectTo: 'head',
             tag: 'script',
             attrs: {
-              src: '/meta-pixel.js' // 외부 스크립트 방식 추천
+             // 외부 스크립트 방식 추천
             }
-          },
-          {
-            injectTo: 'body',
-            tag: 'noscript',
-            children: `
-              <img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=617665268001613&ev=PageView&noscript=1"
-              />
-            `.trim()
           }
         ]
       }
