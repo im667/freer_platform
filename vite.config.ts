c0b2d439ea3e676de180c2226ed506ea7bbaae67
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2025-06-12 19:34:53 +0900

    fix: pixel script fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,44 @@ export default defineConfig({
   plugins: [
     createHtmlPlugin({
       inject: {
-        data: {
-          metaPixel: `
-<!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '617665268001613');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=617665268001613&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-          `.trim()
-        }
+        tags: [
+          {
+            injectTo: 'head', // head에 넣는다
+            tag: 'script',
+            attrs: {
+              // 여기에 src 쓰면 외부파일, 아니면 innerHTML로 삽입
+            },
+            children: `
+              <!-- Meta Pixel Code -->
+          
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '617665268001613');
+              fbq('track', 'PageView');
+           
+            
+              <!-- End Meta Pixel Code -->
+            `.trim()
+          },
+          {
+            injectTo: 'body',
+            tag: 'noscript',
+            children: `
+              <img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=617665268001613&ev=PageView&noscript=1"
+              />
+            `.trim()
+          }
+        ]
       }
     })
   ]
 });
+
+
